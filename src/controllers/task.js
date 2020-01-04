6b6b1f785c5e0f74ba24bdab2937545ce5bbf0cc
Anastasiya <v.anastasiya26@mail.ru>
2020-01-04 19:00:59 +0700

    task-controller 2

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -2,40 +2,55 @@ import TaskComponent from "../components/task";
 import TaskEditComponent from "../components/task-edit";
 import {render, RenderPosition} from "../utils";
 
+const Mode = {
+  DEFAULT: `default`,
+  EDIT: `edit`
+};
+
 export default class TaskController {
-  constructor(container) {
+  constructor(container, onDataChange, onViewChange) {
     this._container = container;
+    this._onDataChange = onDataChange;
+    this._onViewChange = onViewChange;
+
+    this._mode = Mode.DEFAULT;
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
+
+  _replaceEditToTask() {
+    this._container.replaceChild(this._taskComponent.getElement(), this._taskEditComponent.getElement());
+  }
+
+  _replaceTaskToEdit() {
+    this._container.replaceChild(this._taskEditComponent.getElement(), this._taskComponent.getElement());
   }
 
   render(task) {
     const container = this._container;
 
+    // const oldTaskComponent = this._taskComponent;
+    // const oldTaskEditComponent = this._taskEditComponent;
+
     this._taskComponent = new TaskComponent(task);
     this._taskEditComponent = new TaskEditComponent(task);
 
-    const replaceEditToTask = () => {
-      container.replaceChild(this._taskComponent.getElement(), this._taskEditComponent.getElement());
-    };
-
-    const replaceTaskToEdit = () => {
-      container.replaceChild(this._taskEditComponent.getElement(), this._taskComponent.getElement());
-    };
-
-    const onEscKeyDown = (evt) => {
-      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-      if (isEscKey) {
-        replaceEditToTask();
-        document.removeEventListener(`keydown`, onEscKeyDown);
-      }
-    };
-
     this._taskComponent.setEditButtonClickHandler(() => {
-      replaceTaskToEdit();
-      document.addEventListener(`keydown`, onEscKeyDown);
+      this._replaceTaskToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
     });
 
-    this._taskEditComponent.setSubmitHandler(replaceEditToTask);
+    this._taskEditComponent.setSubmitHandler(this._replaceEditToTask);
 
     render(container, this._taskComponent.getElement(), RenderPosition.BEFOREEND);
   }
